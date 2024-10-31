f67b57eaa31ed1b9f15983a243ab437691d8419e
Gasset09 <wortegaq@miumg.edu.gt>
2024-10-30 23:16:02 -0600

    nuevo

--- src/Promocion/promocion.module.ts
+++ src/Promocion/promocion.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PromocionService } from './promocion.service';
 import { PromocionController } from './promocion.controller';
-import { Promocion } from './entities/promocion.entity';
+import { Feedback } from '../Feedback/entities/feedback.entity';
+import { Promocion } from '../Promocion/entities/promocion.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Promocion])],
